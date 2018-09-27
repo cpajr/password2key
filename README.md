dfacd1b152215a713c0536e2a04a9ae2b1ab2374
Charlie Allen <charlie.allenjr@gmail.com>
2018-09-26 21:34:21 -0700

    Updated files and README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# password2key
-This is a python script use to leverage several JARS from Nokia NFM-P password2key.bash.  I am only using this so that it can be ported elsewhere and leveraged by my users.  
+# Password2Key Adaption
+
+##Background
+In deploying a Nokia router, you may have to configure SNMPv3 on it.  For reasons that I still do not understand, Nokia routers require a weird key to be generated for SNMPv3 to be configured.  This key is a hash generated from the combination of the SNMP EngineID and SNMP user password.  To generate this key a Nokia created script (password2key.bash), only found on their NFM-P server, must be executed.  
+
+In my environment, I prefer that my designers and technicians not need to access the NFM-P server to execute this script.  Instead, I prefer that a web interface be available where input can be provided and the needed keys generated.  
+
+##Needed JAR Files
+There are three JAR files that need to be collected for this to work correctly.  They are as follows:
+
+    /opt/nsp/nfmp/server/nms/lib/tools/tools.jar
+    /opt/nsp/nfmp/server/nms/lib/thirdparty/webnms/adventnet/AdventNetSnmp.jar
+    /opt/nsp/nfmp/server/nms/lib/thirdparty/webnms/adventnet/AdventNetLogging.jar
+
+##Script password2key.py
+Place the above JAR files in the same folder as password2key.py.  In my case, in creating a web interface, place them in `/var/www/cgi-bin`.
+
+##Web Input
+I made a simple input website which will call the above Python script.  Place the `index.html` file into `/var/www/html`.     
--- password2key.py
+++ password2key.py
@@ -11,62 +11,60 @@ the router configuration
 
 import os
 import sys
+import cgi,cgitb
+
+#HTML Output
+print("Content-Type: text/html")    
+print()
 
 #Gather user input
+form = cgi.FieldStorage()
 
-print("Gathering IP Address information....")
+#print("Gathering IP Address information....")
 ip_addr1 = "172"
 ip_addr2 = "028"
-ip_addr3 = input("3rd Octet: ")
-ip_addr4 = input("4th Octet: ")
+ip_addr3 = form.getvalue('ip_addr3')
+ip_addr4 = form.getvalue('ip_addr4')
 
 #verfiy IP address input
 try:
     if int(ip_addr3) <= 0 or int(ip_addr3) >=255:
         print()
-        print("ERROR: Not valid 3rd octet IP address.")
-        print("Must be greater than 0 and less than 255")
+        print("ERROR: Not valid 3rd octet IP address.<br><br>")
+        print("Must be greater than 0 and less than 255<br>")
+        print("Return to <a href=\"../index.html\">start page</a>")
         sys.exit()
 
     if int(ip_addr4) <= 0 or int(ip_addr4) >=255:
         print()
-        print("ERROR: Not valid 4th octet IP address")
-        print("Must be greater than 0 and less than 255")
+        print("ERROR: Not valid 4th octet IP address<br><br>")
+        print("Must be greater than 0 and less than 255i<br>")
+        print("Return to <a href=\"../index.html\">start page</a>")
         sys.exit()
 
 except ValueError:
-    print()
-    print("ERROR: An improper number was provided.  Please try again.")
-    print("Exiting...")
+    print("ERROR: An improper number was provided.  Please try again.<br><br>")
+    print("Return to <a href=\"../index.html\">start page</a>")
     sys.exit()
 
 #Print out the System IP Address
 
 system_ip_addr = "172.28." + ip_addr3 + "." + ip_addr4
 
-print("-"*80)
-#print("System IP Address: 172.28.{}.{}".format(ip_addr3, ip_addr4))
-print ("{:>20} {:<30}".format("System IP Address:", system_ip_addr))
-
 #Create EngineID
 lead_zeros = "000000000000"
 
 if int(ip_addr3) < 10:
-	ip_addr3 = "00" + ip_addr3
+        ip_addr3 = "00" + ip_addr3
 elif int(ip_addr3) < 100:
-	ip_addr3 = "0" + ip_addr3
+        ip_addr3 = "0" + ip_addr3
 
 if int(ip_addr4) < 10:
-	ip_addr4 = "00" + ip_addr4
+        ip_addr4 = "00" + ip_addr4
 elif int(ip_addr4) < 100:
-	ip_addr4 = "0" + ip_addr4
+        ip_addr4 = "0" + ip_addr4
 
 engineid = lead_zeros + ip_addr1 + ip_addr2 + ip_addr3 + ip_addr4
-
-#print("EngineID: ", format(engineid))
-print ("{:>20} {:<30}".format("EngineID:", engineid))
-print("-"*80)
-print()
 
 #Needed elements for the password2key execution
 classpath = "tools.jar:AdventNetSnmp.jar:AdventNetLogging.jar"
@@ -83,23 +81,16 @@ except OSError:
     print("Exiting....")
     sys.exit()
 
-
-print("-"*80)
-print("SNMP Write Hash")
-print("-"*80)
+write_sha = ""
+write_aes = ""
 
 for line in output.splitlines():
     hashline = line.split()
     if hashline[0] == "SHA":
-        #print("SHA: {}".format(hashline[2]))
-        print("{:>20} {:<30}".format("SHA:", hashline[2]))
+        write_sha = hashline[2]
 
     if hashline[0] == "AES":
-        #print("AES: {}".format(hashline[2]))
-        print("{:>20} {:<30}".format("AES:", hashline[2]))
-
-print("-"*80)
-print()
+        write_aes = hashline[2]
 
 #Create the Read SNMP hash
 try:
@@ -109,19 +100,50 @@ except OSError:
     print("Exiting....")
     sys.exit()
 
-print("-"*80)
-print("SNMP Read Hash")
-print("-"*80)
-
 for line in output.splitlines():
     hashline = line.split()
     if hashline[0] == "SHA":
-        #print("SHA: {}".format(hashline[2]))
-        print("{:>20} {:<30}".format("SHA:", hashline[2]))
+        read_sha = hashline[2]
 
     if hashline[0] == "AES":
-        #print("AES: {}".format(hashline[2]))
-        print("{:>20} {:<30}".format("AES:", hashline[2]))
+        read_aes = hashline[2]
 
+
+'''
+We're going to print out all of the information
+'''
+print("-" * 80)
+print("<br>")
+print ("{:>20} {:<30} <br>".format("<b>System IP Address</b>:", system_ip_addr))
+print ("{:>20} {:<30}".format("<b>EngineID</b>:", engineid))
+print("<br>")
 print("-"*80)
-print()
+print("<br><br>")
+print("-"*80)
+print("<br>")
+print("<b>SNMP Write Hash</b>")
+print("<br>")
+print("-"*80)
+print("<br>")
+print("{} {}".format("<b>SHA</b>:", write_sha))
+print("<br>")
+print("{} {}".format("<b>AES</b>:", write_aes))
+print("<br>")
+print("-"*80)
+print("<br>")
+print("-"*80)
+print("<br>")
+print("<b>SNMP Read Hash</b>")
+print("<br>")
+print("-"*80)
+print("<br>")
+print("{} {}".format("<b>SHA</b>:", read_sha))
+print("<br>")
+print("{} {}".format("<b>AES</b>:", read_aes))
+print("<br>")
+print("-"*80)
+print("<br>")
+
+
+
+
